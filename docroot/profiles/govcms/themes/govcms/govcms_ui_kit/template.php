2a4a94ea9042b1f58cab170acd8c385043228e25
Kurt Foster <kurt@salsadigital.com.au>
2016-10-10 13:34:40 +1100

    Adding docroot so that it can be deployed on acquia

---
+++ docroot/profiles/govcms/themes/govcms/govcms_ui_kit/template.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * @file
+ * template.php
+ */
+
+/**
+ * Implements hook_html_head_alter().
+ */
+function govcms_ui_kit_html_head_alter(&$head_elements) {
+  // Mobile Viewport.
+  $head_elements['viewport'] = array(
+    '#type' => 'html_tag',
+    '#tag' => 'meta',
+    '#attributes' => array('name' => 'viewport', 'content' => 'width=device-width, initial-scale=1'),
+  );
+  // IE Latest Browser.
+  $head_elements['ie_view'] = array(
+    '#type' => 'html_tag',
+    '#tag' => 'meta',
+    '#attributes' => array('http-equiv' => 'x-ua-compatible', 'content' => 'ie=edge'),
+  );
+}
+
+/**
+ * Implements hook_preprocess_html().
+ */
+function govcms_ui_kit_preprocess_html(&$variables) {
+  drupal_add_js("(function(h) {h.className = h.className.replace('no-js', '') })(document.documentElement);", array('type' => 'inline', 'scope' => 'header'));
+}
+
+/**
+ * Implements hook_preprocess_field().
+ */
+function govcms_ui_kit_preprocess_field(&$variables) {
+  if ($variables['element']['#field_name'] === 'field_slide_image') {
+    // kpr($variables);
+    $variables['items'][0]['#image_style'] = 'govcms_ui_kit_banner';
+  }
+}
+
+/**
+ * Implements hook_image_styles_alter().
+ */
+function govcms_ui_kit_image_styles_alter(&$styles) {
+  $styles['govcms_ui_kit_banner'] = array(
+    'label' => 'govCMS UI-KIT - Banner',
+    'name' => 'govcms_ui_kit_banner',
+    'storage' => IMAGE_STORAGE_NORMAL,
+    'effects' => array(
+      array(
+        'label' => 'Scale and crop',
+        'name' => 'image_scale_and_crop',
+        'data' => array(
+          'width' => 1650,
+          'height' => 440,
+          'upscale' => 1,
+        ),
+        'effect callback' => 'image_scale_and_crop_effect',
+        'dimensions callback' => 'image_resize_dimensions',
+        'form callback' => 'image_resize_form',
+        'summary theme' => 'image_resize_summary',
+        'module' => 'image',
+        'weight' => 0,
+      ),
+    ),
+  );
+  return $styles;
+}
+
+/**
+ * Implements hook_preprocess_node().
+ */
+function govcms_ui_kit_preprocess_node(&$variables) {
+  if ($variables['view_mode'] === 'teaser' || $variables['view_mode'] === 'compact') {
+    $has_thumb = !empty($variables['content']['field_thumbnail']);
+    $has_image = !empty($variables['content']['field_image']);
+    if ($has_thumb || $has_image) {
+      $variables['classes_array'][] = 'has-thumbnail';
+    }
+  }
+}
