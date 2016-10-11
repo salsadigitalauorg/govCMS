67d6b5e7bce00d069147f68ba3d9f20e59675a6d
Kurt Foster <kurt@salsadigital.com.au>
2016-10-12 10:27:59 +1100

    Updating the theme within the profile in docroot

--- docroot/profiles/govcms/themes/govcms/govcms_ui_kit/templates/page.tpl.php
+++ docroot/profiles/govcms/themes/govcms/govcms_ui_kit/templates/page.tpl.php
@@ -34,12 +34,12 @@
       <div class="logo-wrapper">
         <?php if ($logo): ?>
           <?php
-            $logo_alt = theme_get_setting('header_logo_alt');
+            $logo_alt = theme_get_setting('govcms_ui_kit_header_logo_alt');
             $logo_alt = !empty($logo_alt) ? $logo_alt : variable_get('site_name', 'Home');
             $logo_img = theme_image(array(
               'path' => $logo,
               'alt' => $logo_alt,
-              'attributes' => array('class' => array('header__logo-image'))
+              'attributes' => array('class' => array('header__logo-image')),
             ));
             print l($logo_img, $front_page, array(
               'html' => TRUE,
@@ -52,8 +52,8 @@
             ));
           ?>
         <?php endif; ?>
-        <?php if (theme_get_setting('header_title')): ?>
-          <div class="header-title"><?php print decode_entities(theme_get_setting('header_title')); ?></div>
+        <?php if (theme_get_setting('govcms_ui_kit_header_title')): ?>
+          <div class="header-title"><?php print decode_entities(theme_get_setting('govcms_ui_kit_header_title')); ?></div>
         <?php endif; ?>
       </div>
       <?php print render($page['header']); ?>
@@ -73,43 +73,53 @@
 
 <div id="page">
 
+  <?php
+    // Render the sidebars to see if there's anything in them.
+    $sidebar_first  = render($page['sidebar_first']);
+    $sidebar_second = render($page['sidebar_second']);
+  ?>
+
   <?php print render($page['highlighted']); ?>
-  <?php print $breadcrumb; ?>
 
   <div id="main">
     <div id="content" class="column" role="main">
-
-      <a href="#skip-link" id="skip-content" class="element-invisible" tabindex="-1">Go to top of page</a>
-
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
+      <div class="content-header">
+        <div class="content-header-inner">
+          <?php print $breadcrumb; ?>
+          <a href="#skip-link" id="skip-content" class="element-invisible" tabindex="-1">Go to top of page</a>
+          <a id="main-content"></a>
+          <?php print render($title_prefix); ?>
+          <?php if ($title): ?>
+            <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+          <?php endif; ?>
+          <?php print render($title_suffix); ?>
+        </div>
+      </div>
+      <div class="content-body">
+        <?php print $messages; ?>
+        <?php print render($tabs); ?>
+        <?php print render($page['help']); ?>
+        <?php if ($action_links): ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+        <div class="content-body-inner<?php print $sidebar_first ? ' has-sidebar' : ' no-sidebar'; ?>">
+          <?php print render($page['content']); ?>
+          <?php print $feed_icons; ?>
+          <?php if ($sidebar_first): ?>
+            <aside class="sidebar-first">
+              <?php print $sidebar_first; ?>
+            </aside>
+          <?php endif; ?>
+        </div>
+      </div>
     </div>
-
-    <?php
-      // Render the sidebars to see if there's anything in them.
-      $sidebar_first  = render($page['sidebar_first']);
-      $sidebar_second = render($page['sidebar_second']);
-    ?>
-
-    <?php if ($sidebar_first || $sidebar_second): ?>
-      <aside class="sidebars" role="complementary">
-        <?php print $sidebar_first; ?>
-        <?php print $sidebar_second; ?>
-      </aside>
-    <?php endif; ?>
   </div>
+
+  <?php if ($sidebar_second): ?>
+    <aside class="sidebar-second" role="complementary">
+      <?php print $sidebar_second; ?>
+    </aside>
+  <?php endif; ?>
   
   <div id="footer">
     <?php print render($page['footer']); ?>
--- docroot/profiles/govcms/themes/govcms/govcms_ui_kit/theme-settings.php
+++ docroot/profiles/govcms/themes/govcms/govcms_ui_kit/theme-settings.php
@@ -8,8 +8,8 @@
 /**
  * Implements hook_system_theme_settings_alter().
  */
-function govcms_uikit_form_system_theme_settings_alter(&$form, $form_state) {
-  $form['govcms_uikit_options'] = array(
+function govcms_ui_kit_form_system_theme_settings_alter(&$form, $form_state) {
+  $form['govcms_ui_kit_options'] = array(
     '#type' => 'fieldset',
     '#title' => t('govCMS UI Kit settings'),
     '#weight' => 5,
@@ -17,24 +17,31 @@ function govcms_uikit_form_system_theme_settings_alter(&$form, $form_state) {
     '#collapsed' => FALSE,
   );
 
-  $form['govcms_uikit_options']['header_title'] = array(
+  $form['govcms_ui_kit_options']['govcms_ui_kit_header_title'] = array(
     '#type'          => 'textfield',
     '#title'         => t('Header title'),
-    '#default_value' => theme_get_setting('header_title'),
+    '#default_value' => theme_get_setting('govcms_ui_kit_header_title'),
     '#description'   => t("Text to display beside the site logo in the top header."),
   );
 
-  $form['govcms_uikit_options']['header_logo_alt'] = array(
+  $form['govcms_ui_kit_options']['govcms_ui_kit_header_logo_alt'] = array(
     '#type'          => 'textfield',
     '#title'         => t('Header logo alternative text'),
-    '#default_value' => theme_get_setting('header_logo_alt'),
+    '#default_value' => theme_get_setting('govcms_ui_kit_header_logo_alt'),
     '#description'   => t("Alternative text to assign to the logo in the top header."),
   );
 
-  $form['govcms_uikit_options']['footer_copyright'] = array(
+  $form['govcms_ui_kit_options']['govcms_ui_kit_footer_copyright'] = array(
     '#type'          => 'textfield',
     '#title'         => t('Footer copyright'),
-    '#default_value' => theme_get_setting('footer_copyright'),
+    '#default_value' => theme_get_setting('govcms_ui_kit_footer_copyright'),
     '#description'   => t("Text to display beside the sub menu links. Defaults to <em>&copy; [current year]. [Site Name]. All rights reserved.</em>"),
+  );
+
+  $form['govcms_ui_kit_options']['govcms_ui_kit_override_image_styles'] = array(
+    '#type'          => 'checkbox',
+    '#title'         => t('Override image styles'),
+    '#default_value' => theme_get_setting('govcms_ui_kit_override_image_styles'),
+    '#description'   => t("Enable this to override any user-defined image styles with govCMS UI Kit default styles. Disabling this is recommend if modifying site."),
   );
 }
