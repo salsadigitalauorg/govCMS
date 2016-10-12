4b9e583faa451410f4d650e65db8ec463e8b1eeb
Kurt Foster <kurt@salsadigital.com.au>
2016-10-12 13:44:39 +1100

    Removing the profiles/themes/govcms folder

--- docroot/profiles/govcms/themes/govcms/govcms_ui_kit/vendor/gov-au-ui-kit/pa11y.js
+++
@@ -1,9 +0,0 @@
-// This is our options configuration for Pa11y
-// https://github.com/pa11y/pa11y#configuration
-
-const options = {
-  timeout: 60000,
-  hideElements: '.skip-to, .is-visuallyhidden, .visuallyhidden'
-};
-
-module.exports = options;
