db3d01d0244612c899c271e0c745839805669f48
Kurt Foster <kurt@salsadigital.com.au>
2016-10-23 17:16:20 +1030

    Adding some Acquia hooks

---
+++ hooks/prod/post-code-deploy/clear-varnish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# site="$1"
+# target_env="$2"
+# source_branch="$3"
+# deployed_tag="$4"
+# repo_url="$5"
+# repo_type="$6"
+
+# drush @$target_env  ac-domain-purge <domain.com>
+# drush @$target_env cc all
---
+++ hooks/uat/post-code-deploy/clear-varnish.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../../prod/post-code-deploy.sh $1 $2 $3 $4 $5 $6
