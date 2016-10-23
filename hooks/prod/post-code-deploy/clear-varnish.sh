4db87b04ea8cd33a6637871933eccec9ec923ed5
Kurt Foster <kurt@salsadigital.com.au>
2016-10-23 17:32:23 +1030

    Uncommenting the variables

--- hooks/prod/post-code-deploy/clear-varnish.sh
+++ hooks/prod/post-code-deploy/clear-varnish.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# site="$1"
-# target_env="$2"
-# source_branch="$3"
-# deployed_tag="$4"
-# repo_url="$5"
-# repo_type="$6"
+site="$1"
+target_env="$2"
+source_branch="$3"
+deployed_tag="$4"
+repo_url="$5"
+repo_type="$6"
 
-drush @$target_env ac-domain-purge govcmsnewthemeqc5gsdrz6m.devcloud.acquia-sites.com
+drush @$site.$target_env ac-domain-purge govcmsnewthemeqc5gsdrz6m.devcloud.acquia-sites.com
 drush @$target_env cc all
