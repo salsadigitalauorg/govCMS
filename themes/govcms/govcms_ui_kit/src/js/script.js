ed5571ad8c748fec89fa3082f4a5a7a010404dd7
Alan <alan@salsadigital.com.au>
2017-07-03 14:09:30 +1000

    [GOVCMSTHM-66] - Validation fixes
    * Split validation code to new file
    * Add email example in error message (with other disabled error message updates)
    * Fix to spelling mistakes in comments
    * Cleaned up jQuery requests, fixed bug with messages not working on contact form

--- themes/govcms/govcms_ui_kit/src/js/script.js
+++ themes/govcms/govcms_ui_kit/src/js/script.js
@@ -17,23 +17,6 @@ var desktop_column = 1170;
     attach: function(context, settings) {
       // Object Fit Polyfill for IE. Used on News Teaser Images.
       objectFitImages();
-
-      // Webform validation.
-      $('form.webform-client-form, form.contact-form', context).validate({
-        errorElement: 'span',
-        errorPlacement: function(error, element) {
-          // Plave error msg within field label.
-          error.appendTo(element.parents('.form-item.webform-component, form.contact-form .form-item').find('> label'));
-        },
-        showErrors: function(errorMap, errorList) {
-          // Remove asterik and display custom markup for error.
-          $(errorList).each(function() {
-            $(this.element).parents('.form-item.webform-component, form.contact-form .form-item').find('> label .form-required').remove();
-            this.message = '(Error - ' + this.message + ')';
-          });
-          this.defaultShowErrors();
-        }
-      });
     }
   };
 
